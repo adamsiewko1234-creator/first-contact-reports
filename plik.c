744583af3e8729fa9b2103c761c9c3db3e6cb67a
Adam Siewko <adamsiewko1234@gmail.com>
2026-01-10 21:46:33 +0100

    dodano obsluge wczytywania z pliku w pliku plik.c

--- plik.c
+++ plik.c
@@ -4,7 +4,8 @@
 void zapisanieDoPliku(const ListaImplantow *lista, const char *plikImplanty){
     FILE *plik = fopen(plikImplanty.txt, "w");
     if(!plik){
-        return
+        printf("Przy zapisywaniu do pliku wystapil blad.\n");
+        return;
     }
 
     ElementListy *element = lista -> elementPierwszy;
@@ -20,5 +21,32 @@ void zapisanieDoPliku(const ListaImplantow *lista, const char *plikImplanty){
         element = element -> kolejny;
     }
     fclose(plik);
+}
 
+void wczytanieZPliku(ListaImplantow *lista, const char *plikImplanty){
+    FILE *plik = fopen(plikImplanty.txt, "r");
+    if(!plik){
+        printf("Wystapil blad przy odczycie danych z pliku.\n");
+        return;
+    }
+
+    zwolnienieListy(lista);
+    tworzenieListy(lista);
+
+    Implant tymczasowy;
+    int status;
+
+    while(fscanf(plik, "%99[^;];%49[^;];%99[^;];%d;%f;%d\n",
+        tymczasowy.nazwaImplantu,
+        tymczasowy.idWlasciciela,
+        tymczasowy.producent,
+        &tymczasowy.poziomRyzyka,
+        &tymczasowy.zapotrzebowanieEnergii,
+        &status
+    ) == 6){
+        tymczasowy.legalnoscImplantu = (StatusLegalnosci)status;
+        dodawanieImplantu(lista, tymczasowy);
+    }
+    fclose(plik);
+    printf("Pomyslnie wczytano dane z pliku. \n");
 }
